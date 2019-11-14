b2d18da37dcc7596e6caaa36a9d90543e51e1aa6
chanwoo <ch6845@naver.com>
2019-11-14 21:31:16 +0900

    backup

---
+++ .py
@@ -0,0 +1,28 @@
+import subprocess
+
+from path_configure import *
+
+def run_subprocess(command,quiet=False):
+    print("------{}-----".format("RUN"))
+    print(command)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if quiet==False:
+        print("------{}-----".format("ERROR"))
+        print(stderr.decode())
+        print("------{}-----".format("OUTPUT"))
+        print(stdout.decode())
+    return stdout,stderr
+
+
+
+def replace_characters(str_input,character_dict,catch_exception=True):
+    for to_replace,value in character_dict.items():
+        if catch_exception:
+            try:
+                str_input=str_input.replace(to_replace,value)
+            except:
+                pass
+        else:
+            str_input=str_input.replace(to_replace,value)
+    return str_input  
--- README.md
+++ README.md
@@ -4,13 +4,31 @@
 English: http://www.nih.go.kr/menu.es?mid=a50303010100
 Korean: http://www.nih.go.kr/menu.es?mid=a40504010000
 
+## Datasource
+### Cohort
+### Region-based(Ansan, Ansung)
+Number
+### City
+### Rural
+### Total: 12x,xxx
+
+### Genotype data
+SNP: xx
+
 ## MHC region genotype imputation
 ### China asia pan-a
+markers=
+N=
 ### CookHLA
+citation
 ### SNP2HLA
+citation
+
+include cohort in associationt only if case in cohort>0 (questionnaire exists)
 
 ## Phenotype Curation
 ### Automatic cohort merge manual syntax analyzer
+other github repo
 
 ## Association test
 
@@ -22,10 +40,16 @@ https://www.ncbi.nlm.nih.gov/snp/?term=6%5BChromosome%5D+AND+1500000%3A3000000%5
 ftp://ftp.ncbi.nih.gov/snp/latest_release
 
 ## Plotting
+code
 
-## gcta h2
 
-## gcta correlation
+## GCTA h2
+
+## GCTA correlation
+
+
+
+## Citation
 
 
 
